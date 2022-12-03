b4d7f345907ad712d24dad1ff9187d08cee174b0
Ivan Lozovskij <ivan.lozovskij1@gmai.com>
2022-12-03 17:02:48 +0300

    feat: add login-page

--- library-app/src/app/app-routing.module.ts
+++ library-app/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BooksCatalogComponent } from './books-catalog/books-catalog.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: '', component: BooksCatalogComponent },
+  { path: 'catalog', component: BooksCatalogComponent },
   { path: 'books/:bookId', component: BookDetailsComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginPageComponent },
 ];
 
 @NgModule({
--- library-app/src/app/app.module.ts
+++ library-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BooksCatalogComponent } from './books-catalog/books-catalog.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { ProfileComponent } from './profile/profile.component';
     TopBarComponent,
     BookDetailsComponent,
     ProfileComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
