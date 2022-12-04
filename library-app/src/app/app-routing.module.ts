968b60c8b4d39fd0cb1fd9a85c2c8a369392c6d5
Ivan Lozovskij <ivan.lozovskij1@gmai.com>
2022-12-04 15:56:20 +0300

    feat: redirect to login (canActivate)

--- library-app/src/app/app-routing.module.ts
+++ library-app/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BooksCatalogComponent } from './books-catalog/books-catalog.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { CanActivatePatron } from './patron-guard';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: 'catalog', component: BooksCatalogComponent },
+  { path: 'catalog', component: BooksCatalogComponent, canActivate:[ CanActivatePatron ] },
   { path: 'books/:bookId', component: BookDetailsComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginPageComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [CanActivatePatron]
 })
 export class AppRoutingModule { }
---
+++ library-app/src/app/patron-guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class CanActivatePatron implements CanActivate {
+    constructor(
+        private router: Router,
+      ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {
+    if (localStorage.getItem('USER_TYPE') === 'patron'){
+        return true;
+    } else {
+        this.router.navigate(['/login']);
+        return false;
+    }
+  }
+}
